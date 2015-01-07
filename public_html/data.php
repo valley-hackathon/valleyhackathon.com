ba57a8f18d8784adcb962d2ccd799b4e786bb0ea
Nathan Bunney <nathan@Inventaweb.net>
2015-01-07 11:23:38 -0800

    Squashed commit of the following:
    commit 9149055d05c1b3b09d3c66b623dd2d42ef43ee7e
    Author: Nathan Bunney <nathan@Inventaweb.net>
    Date:   Wed Jan 7 11:19:56 2015 -0800
    
        Updates to sponsors and judges pages

--- public_html/data.php
+++ public_html/data.php
@@ -24,10 +24,14 @@ $data['judges'][] = array(
 $data['judges'][] = array(
   'Name'    => 'Erin Priest',
 <<<<<<< HEAD
+<<<<<<< HEAD
   'Bio'     => 'Web and Graphic Designer at Marcia Herrmann Design, Erin has been doing web design for a lifetime.',
 =======
   'Bio'     => 'Web and Graphic Designer at Marcia Herrmann Design, Erin has been doing design for a lifetime.',
 >>>>>>> Updates to sponsors and judges pages
+=======
+  'Bio'     => 'Web and Graphic Designer at Marcia Herrmann Design, Erin has been doing web design for a lifetime.',
+>>>>>>> Squashed commit of the following:
   'picture' => 'ErinPriest.jpg',
   'sponsorID' => 7
 );
