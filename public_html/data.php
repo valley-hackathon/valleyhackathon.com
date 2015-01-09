af1971f8afc8ac8f8967d3124126bf143be302bb
Nathan Bunney <nathan@Inventaweb.net>
2015-01-08 16:36:05 -0800

    removed swift from stormagedden

--- public_html/data.php
+++ public_html/data.php
@@ -160,7 +160,7 @@ $data['sponsorship'][] = array(
  */
 $data['teams'][] = array(
   'Name'       => 'Project Stormageddon',
-  'Frameworks' => 'Angular.js / Slim / Swift',
+  'Frameworks' => 'Angular.js / Slim',
   'Members'    => 'Matthew Davies<br>Nathan Bunney<br>Robert Huffman<br>Jared Hill',
   'County'     => 'Stanislaus'
 );
